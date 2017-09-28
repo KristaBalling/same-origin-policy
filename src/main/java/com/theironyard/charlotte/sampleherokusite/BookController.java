5deacd78709c41fd97312a3ccb8b4856b36ad9a7
Krista Blachian <KristaBlachian@outlook.com>
2017-09-28 16:00:47 -0400

    Initial Commit

---
+++ src/main/java/com/theironyard/charlotte/sampleherokusite/BookController.java
@@ -0,0 +1,50 @@
+package com.theironyard.charlotte.sampleherokusite;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class BookController {
+
+    List<Book> books = new ArrayList<>();
+
+    @CrossOrigin
+    @RequestMapping(path = "/books", method = RequestMethod.GET)
+    public List<Book> getBooks() {
+//        Book greenEggsAndHam = new Book("1234","Green Eggs And Ham",
+//                                        "Dr. Seuss!", true,
+//                            "Awesome", true, 67 );
+//        books.add(greenEggsAndHam);
+        return books;
+    }
+    // {"isbn":"12345","title":"The Great Gatsomething","author":"yamama"}
+
+    //    @CrossOrigin(origins = {"http://thegoogs.com", "yahoo.com", "idk.com"})
+    @CrossOrigin
+    @RequestMapping(path = "/books", method = RequestMethod.POST)
+    public void addBook(@RequestBody Book book) {
+        books.add(book);
+
+    }
+
+    @CrossOrigin
+    @RequestMapping(path = "/checking_out/{index}", method = RequestMethod.PATCH)
+    public void checkOut(@PathVariable("index") int index) {
+        if (books.get(index).isCheckedOut() == false) {
+            books.get(index).setCheckedOut(true);
+        }
+    }
+
+    @CrossOrigin
+    @RequestMapping(path="/checking_in/{index}", method = RequestMethod.PATCH)
+    public void checkIn(@PathVariable("index") int index) {
+        if(books.get(index).isCheckedOut() == true); {
+            books.get(index).setCheckedOut(false);
+        }
+
+    }
+}
+
